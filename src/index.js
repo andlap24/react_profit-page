dd60b52ca942caab613fa2500d4567f1f6bfa2d9
andlap24 <andrew.laptev24@gmail.com>
2020-12-01 17:35:25 +0200

    delete router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { HashRouter as Router } from 'react-router-dom';
 import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
